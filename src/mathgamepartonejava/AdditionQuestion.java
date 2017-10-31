5fe2aeffb75e68eea7e944de8f9fd4c6605c2003
J.K <K_940201@hotmail.com>
2017-10-31 04:07:57 -0700

    create AdditionQuestion class

---
+++ src/mathgamepartonejava/AdditionQuestion.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mathgamepartonejava;
+
+/**
+ *
+ * @author j.k.
+ */
+public class AdditionQuestion {
+    string question;
+}
